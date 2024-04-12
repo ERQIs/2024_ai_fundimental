e6f6fad9dd5f7de48f764c5af4b4c308e44d5cc4
ERQIs <118714734+ERQIs@users.noreply.github.com>
2024-04-12 18:55:18 +0800

    Create make.py

---
+++ Prob3/make.py
@@ -0,0 +1,5 @@
+import os;
+os.system("g++ -o ./bin/bfs ./src/BFS.cc")
+os.system("g++ -o ./bin/generator ./src/generator.cc")
+os.system("g++ -o ./bin/dijkstra ./src/dijkstra.cc")
+os.system("g++ -o ./bin/A_star ./src/A_star.cc")
